25fa24342989ef6d98b8d133be5ed0703b6e680b
christopheracollett <christopheracollett@gmail.com>
2020-10-29 13:08:06 -0600

    Removed unnecessary conditional functions.

--- Gilded_Rose.py
+++ Gilded_Rose.py
@@ -20,29 +20,14 @@ class Item:
         return "%s, %s, %s" % (self.name, self.sell_in, self.quality)
 
     def update_quality(self):
-        if self.quality_over_0():
+        if self.quality > 0:
             self.decrement_quality()
         self.decrement_sell_in()
-        if self.sell_in_under_0() and self.quality_over_0():
+        if self.sell_in < 0 and self.quality > 0:
             self.decrement_quality()
-
-    def sell_in_under_0(self):
-        return self.sell_in < 0
-
-    def sell_in_under_6(self):
-        return self.sell_in < 6
-
-    def sell_in_under_11(self):
-        return self.sell_in < 11
 
     def decrement_sell_in(self):
         self.sell_in -= 1
-
-    def quality_over_0(self):
-        return self.quality > 0
-
-    def quality_under_50(self):
-        return self.quality < 50
 
     def increment_quality(self):
         self.quality += 1
@@ -56,23 +41,23 @@ class Item:
 
 class Item_Aged_Brie(Item):
     def update_quality(self):
-        if self.quality_under_50():
+        if self.quality < 50:
             self.increment_quality()
         self.decrement_sell_in()
-        if self.sell_in_under_0() and self.quality_under_50():
+        if self.sell_in < 0 and self.quality < 50:
             self.increment_quality()
 
 
 class Item_Backstage_Passes(Item):
     def update_quality(self):
-        if self.quality_under_50():
+        if self.quality < 50:
             self.increment_quality()
-            if self.sell_in_under_11() and self.quality_under_50():
+            if self.sell_in < 11 and self.quality < 50:
                 self.increment_quality()
-            if self.sell_in_under_6() and self.quality_under_50():
+            if self.sell_in < 6 and self.quality < 50:
                 self.increment_quality()
         self.decrement_sell_in()
-        if self.sell_in_under_0():
+        if self.sell_in < 0:
             self.reset_quality()
 
 
