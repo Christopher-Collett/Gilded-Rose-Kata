9e1c9c1e4974e9f4509ca7fabbd57c1f398a408d
christopheracollett <christopheracollett@gmail.com>
2020-10-29 15:11:39 -0600

    Updated tests and classes to clearly test/implement rules.

--- Gilded_Rose.py
+++ Gilded_Rose.py
@@ -7,63 +7,87 @@ class GildedRose(object):
         self.items = items
 
     def update_quality(self):
-        [item.update_quality() for item in self.items]
+        [item.update_item() for item in self.items]
 
 
 class Item:
+
     def __init__(self, name, sell_in, quality):
         self.name = name
         self.sell_in = sell_in
         self.quality = quality
 
+        self.min_quality = 0
+        self.max_quality = 50
+        self.original_quality_delta = self.quality_delta = -1
+
+        self.sell_in_delta = -1
+
     def __repr__(self):
         return "%s, %s, %s" % (self.name, self.sell_in, self.quality)
 
+    def update_item(self):
+        self.update_sell_in()
+        self.update_quality()
+
     def update_quality(self):
-        if self.quality > 0:
-            self.decrement_quality()
-        self.decrement_sell_in()
-        if self.sell_in < 0 and self.quality > 0:
-            self.decrement_quality()
+        self.quality += self.quality_delta
+        self.check_quality()
 
-    def decrement_sell_in(self):
-        self.sell_in -= 1
+    def check_quality(self):
+        self.quality = self.get_in_range(self.quality,
+                                         self.min_quality, self.max_quality)
 
-    def increment_quality(self):
-        self.quality += 1
+    def update_sell_in(self):
+        self.sell_in += self.sell_in_delta
+        self.check_sell_in()
 
-    def decrement_quality(self):
-        self.quality -= 1
+    def check_sell_in(self):
+        if self.sell_in < 0:
+            self.quality_delta = self.original_quality_delta * 2
 
-    def reset_quality(self):
-        self.quality = 0
+    def get_in_range(self, value, min_value, max_value):
+        if value < min_value:
+            return min_value
+        elif value > max_value:
+            return max_value
+        return value
 
 
 class Item_Aged_Brie(Item):
-    def update_quality(self):
-        if self.quality < 50:
-            self.increment_quality()
-        self.decrement_sell_in()
-        if self.sell_in < 0 and self.quality < 50:
-            self.increment_quality()
+    def __init__(self, name, sell_in, quality):
+        super().__init__(name, sell_in, quality)
+
+        self.original_quality_delta = self.quality_delta = 1
 
 
 class Item_Backstage_Passes(Item):
-    def update_quality(self):
-        if self.quality < 50:
-            self.increment_quality()
-            if self.sell_in < 11 and self.quality < 50:
-                self.increment_quality()
-            if self.sell_in < 6 and self.quality < 50:
-                self.increment_quality()
-        self.decrement_sell_in()
+    def __init__(self, name, sell_in, quality):
+        super().__init__(name, sell_in, quality)
+
+        self.original_quality_delta = self.quality_delta = 1
+        self.quality_after_sell_in_past = 0
+
+    def check_sell_in(self):
+        if self.sell_in <= 10:
+            self.quality_delta = self.original_quality_delta * 2
+        if self.sell_in <= 5:
+            self.quality_delta = self.original_quality_delta * 3
         if self.sell_in < 0:
-            self.reset_quality()
+            self.quality_delta = 0
+            self.quality = self.quality_after_sell_in_past
 
 
 class Item_Sulfuras(Item):
-    def update_quality(self):
-        pass
+    def __init__(self, name, sell_in, quality):
+        super().__init__(name, sell_in, quality)
+
+        self.min_quality = 80
+        self.max_quality = 80
+        self.quality_delta = 0
+
+        self.sell_in = 0
+        self.sell_in_delta = 0
 
 
 def make_item(name, sell_in, quality):
--- test_Gilded_Rose.py
+++ test_Gilded_Rose.py
@@ -15,11 +15,71 @@ def get_test_cases(filename):
     return test_cases
 
 
+def test_quality_and_sell_in_decrease_normally():
+    test_before_and_after('+5 Dexterity Vest', 5, 5, 4, 4)
+
+
+def test_quality_decreases_by_2_after_sell_in_date():
+    test_before_and_after('+5 Dexterity Vest', 0, 5, -1, 3)
+
+
+def test_quality_isnt_negative():
+    test_before_and_after('+5 Dexterity Vest', 5, 0, 4, 0)
+
+
+def test_aged_brie_increases_in_quality():
+    test_before_and_after('Aged Brie', 5, 10, 4, 11)
+
+
+def test_aged_brie_doesnt_go_over_50():
+    test_before_and_after('Aged Brie', 5, 50, 4, 50)
+
+
+def test_sulfuras_doesnt_change():
+    test_before_and_after('Sulfuras, Hand of Ragnaros',
+                          0, 80, 0, 80)
+
+
+def test_backstage_increases_in_quality():
+    test_before_and_after('Backstage passes to a TAFKAL80ETC concert',
+                          20, 10, 19, 11)
+
+
+def test_backstage_doesnt_go_over_50():
+    test_before_and_after('Backstage passes to a TAFKAL80ETC concert',
+                          5, 50, 4, 50)
+
+
+def test_backstage_increase_by_2_on_day_10():
+    test_before_and_after('Backstage passes to a TAFKAL80ETC concert',
+                          11, 10, 10, 12)
+
+
+def test_backstage_increase_by_2_after_day_10():
+    test_before_and_after('Backstage passes to a TAFKAL80ETC concert',
+                          10, 10, 9, 12)
+
+
+def test_backstage_increase_by_3_on_day_5():
+    test_before_and_after('Backstage passes to a TAFKAL80ETC concert',
+                          6, 10, 5, 13)
+
+
+def test_backstage_increase_by_3_after_day_5():
+    test_before_and_after('Backstage passes to a TAFKAL80ETC concert',
+                          5, 10, 4, 13)
+
+
+def test_backstage_quality_is_0_after_day_0():
+    test_before_and_after('Backstage passes to a TAFKAL80ETC concert',
+                          0, 10, -1, 0)
+
+
 @pytest.mark.parametrize('name, sell_in, quality,' +
                          'expected_sell_in, expected_quality',
                          get_test_cases('Gilded_Rose_Example.csv'))
-def test_predefined_quality_standard(name, sell_in, quality,
-                                     expected_sell_in, expected_quality):
+def test_before_and_after(name, sell_in, quality,
+                          expected_sell_in, expected_quality):
     item = make_item(name, sell_in, quality)
     gilded_rose = GildedRose([item])
     gilded_rose.update_quality()
